14e7f826906840e04b78fbe0e029d3ca095afd7a
NickDIma101723 <101723@glr.nl>
2025-01-13 22:13:13 +0000

    Food menu is done

--- home page/js/script.js
+++ home page/js/script.js
@@ -1,13 +1,13 @@
-// Get the image and the ID display box
+
 const image = document.getElementById("clickImage");
 const idBox = document.getElementById("box");
 
-// Add an event listener for the image click
+
 image.addEventListener("click", function() {
-  // Toggle the display of the ID box
+
   if (idBox.style.display === "none" || idBox.style.display === "") {
-    idBox.style.display = "block"; // Show the ID box
+    idBox.style.display = "block"; 
   } else {
-    idBox.style.display = "none"; // Hide the ID box
+    idBox.style.display = "none"; 
   }
 });
